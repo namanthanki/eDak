4443a8480736d1e0ba50aeff7c09bdb6675ee621
ThankiAshish <thankiashish555@gmail.com>
2022-02-18 14:26:29 +0530

    Individual Add Friend Component

--- client/src/screens/components/AddFriend.jsx
+++ client/src/screens/components/AddFriend.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 import * as moment from "moment";
 
-import Modal from "./Modal";
+import { isAuth } from "../../helpers/auth";
+import { ChatState } from "../../context/ChatProvider.jsx";
 
 const AddFriend = ({ user }) => {
+  const { setComponent, setSelectedChat, chats, setChats } = ChatState();
+
+  const history = useHistory();
+
+  const createChat = async (userId) => {
+    try {
+      const id = isAuth()._id;
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        `http://localhost:5000/user/chat/${id}/`,
+        { userId },
+        config
+      );
+      setComponent("WriteLetter");
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+      setSelectedChat(data);
+      history.push("/app");
+    } catch (err) {
+      console.log(err.message);
+      toast.error("Error Fetching Chat Data");
+    }
+  };
   return (
-    <div className="add-friend" onClick={() => <Modal />}>
+    <div className="add-friend" onClick={() => createChat(user._id)}>
       <div className="friend-image">
         <img src={user.userProfileImage} alt="friend" />
       </div>
