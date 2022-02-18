685d987a3f80cc5fe22742186db606639a1afe62
ThankiAshish <thankiashish555@gmail.com>
2022-02-18 14:24:53 +0530

    Added User Location

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -2,41 +2,37 @@ import errorHandler from "../helpers/databaseErrorHandling.js";
 
 import dotenv from "dotenv";
 dotenv.config({
-    path: "./config/config.env"
+  path: "./config/config.env",
 });
 
 import User from "../models/User.js";
 
 const profileController = async (req, res) => {
-    const { id } = req.params;
-    await User
-        .findById(id)
-        .exec((err, user) => {
-            if (err || !user) {
-                return res.status(400).json({
-                    error: 'User does not exist, Please Create Account'
-                });
-            }
+  const { id } = req.params;
+  await User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User does not exist, Please Create Account",
+      });
+    }
 
-            const { userProfileImage } = user;
-
-            return res.json({
-                user: { userProfileImage }
-            })
-        });
-}
+    res.send(user);
+  });
+};
 
 const searchUserController = async (req, res) => {
-    const { id } = req.params;
-    const keyword = req.query.username ? {
+  const { id } = req.params;
+  const keyword = req.query.username
+    ? {
         $or: [
-            { username: {$regex: req.query.username, $options: "i"} },
-            { email: {$regex: req.query.username, $options: "i"} }
-        ]
-    } : {}
+          { username: { $regex: req.query.username, $options: "i" } },
+          { email: { $regex: req.query.username, $options: "i" } },
+        ],
+      }
+    : {};
 
-    const users = await User.find(keyword).find({_id: {$ne: id}});
-    res.send(users);
-}
+  const users = await User.find(keyword).find({ _id: { $ne: id } });
+  res.send(users);
+};
 
 export { profileController, searchUserController };
--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -2,7 +2,7 @@ import _ from "lodash";
 
 import dotenv from "dotenv";
 dotenv.config({
-    path: "./config/config.env"
+  path: "./config/config.env",
 });
 
 import errorHandler from "../helpers/databaseErrorHandling.js";
@@ -16,102 +16,100 @@ const from_email = process.env.FROM_EMAIL;
 const email_pass = process.env.EMAIL_PASS;
 
 const registerController = async (req, res) => {
-    const { 
-        username,
-        email,
-        password,
-        bio,
-        gender,
-        dateOfBirth,
-        languages,
-        interests
-    } = req.body;
-    const errors = validationResult(req);
+  const {
+    username,
+    email,
+    password,
+    bio,
+    gender,
+    dateOfBirth,
+    location,
+    languages,
+    interests,
+  } = req.body;
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        const firstError = errors.array().map(error => error.msg)[0];
-        return res.status(422).json({
-            errors: firstError
-        });
-    } else {
-        await User.findOne().or(
-            [
-                { email },
-                { username }
-            ]
-        ).exec((err, user) => {
-            if (user) {
-                return res.status(400).json({
-                    errors: 'Email or Username is taken'
-                });
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map((error) => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError,
+    });
+  } else {
+    await User.findOne()
+      .or([{ email }, { username }])
+      .exec((err, user) => {
+        if (user) {
+          return res.status(400).json({
+            errors: "Email or Username is taken",
+          });
+        } else {
+          const token = jwt.sign(
+            {
+              username,
+              email,
+              password,
+              bio,
+              gender,
+              dateOfBirth,
+              location,
+              languages,
+              interests,
+            },
+            process.env.JWT_ACCOUNT_ACTIVATION,
+            {
+              expiresIn: "1d",
+            }
+          );
+
+          const transport = {
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: false,
+            ingoreTLS: false,
+            service: "Gmail",
+            auth: {
+              user: from_email,
+              pass: email_pass,
+            },
+          };
+
+          const transporter = nodemailer.createTransport(transport);
+
+          transporter.verify((error, success) => {
+            if (error) {
+              console.log(error);
             } else {
-                const token = jwt.sign(
-                    {
-                        username,
-                        email,
-                        password,
-                        bio,
-                        gender,
-                        dateOfBirth,
-                        languages,
-                        interests
-                    },
-                    process.env.JWT_ACCOUNT_ACTIVATION,
-                    {
-                        expiresIn: '1d'
-                    }
-                );
-        
-                const transport = {
-                    host: 'smtp.gmail.com',
-                    port: 465,
-                    secure: false,
-                    ingoreTLS: false,
-                    service: "Gmail",
-                    auth: {
-                        user: from_email,
-                        pass: email_pass
-                    }
-                };
-        
-                const transporter = nodemailer.createTransport(transport);
-        
-                transporter.verify((error, success) => {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Server is ready to take messages');
-                    }
-                });
-        
-                const emailData = {
-                    from: process.env.FROM_EMAIL,
-                    to: email,
-                    subject: 'Account Activation Link',
-                    html:
-                        `
+              console.log("Server is ready to take messages");
+            }
+          });
+
+          const emailData = {
+            from: process.env.FROM_EMAIL,
+            to: email,
+            subject: "Account Activation Link",
+            html: `
                         <h1>Click the link to activate your account</h1>
                         <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
                         <hr>
                         <p>This email contains sensitive information</p>
                         <p>${process.env.CLIENT_URL}</p>
-                    `
-                };
-        
-                transporter.sendMail(emailData, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-        
-                        return res.json({
-                            message: `Email has been sent to ${email}`,
-                        });
-                    }
-                });
+                    `,
+          };
+
+          transporter.sendMail(emailData, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Email sent: " + info.response);
+
+              return res.json({
+                message: `Email has been sent to ${email}`,
+              });
             }
-        });
-    }
+          });
+        }
+      });
+  }
 };
 
 export { registerController };
