9fd80fb4b9742e9c1ee4f42a430273e2e988385b
Naman Thanki <namanthanki@users.noreply.github.com>
2022-03-26 20:57:44 +0530

    Profile Settings Update Controllers

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { profileController } from "../controllers/profileController.js";
+import { deleteFriendChat, profileController, updateUserBio, updateUserInterests, updateUserLanguages } from "../controllers/profileController.js";
 import { searchUserController } from "../controllers/profileController.js";
 
 const router = express.Router();
 
 router.get("/:id", profileController);
 router.get("/:id/search", searchUserController);
+router.patch("/:id/update/interests", updateUserInterests);
+router.patch("/:id/update/languages", updateUserLanguages);
+router.patch("/:id/update/bio", updateUserBio);
+router.delete("/:id/chat/delete", deleteFriendChat);
 
 export default router;
